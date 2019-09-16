7bdb77cd6d54c0d9307bf38506b1edcb791d314f
tetrapod117 <tetrapod117@gmail.com>
2019-09-17 00:16:04 +0900

    change USER_SERVICE_UUID

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '80821c2a-c75e-4dce-afb3-b546925ad8c0'; 
+const USER_SERVICE_UUID         = '{YOUR_SERVICE_UUID}'; 
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
