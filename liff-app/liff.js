c409cdcff801edc7c61a1ff6e5ef2fd62c964353
tetrapod117 <tetrapod117@gmail.com>
2019-09-17 00:14:20 +0900

    Add liff-app

---
+++ liff-app/liff.js
@@ -0,0 +1,257 @@
+// User service UUID: Change this to your generated service UUID
+const USER_SERVICE_UUID         = '80821c2a-c75e-4dce-afb3-b546925ad8c0'; 
+// User service characteristics
+const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
+const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
+
+// PSDI Service UUID: Fixed value for Developer Trial
+const PSDI_SERVICE_UUID         = 'E625601E-9E55-4597-A598-76018A0D293D'; // Device ID
+const PSDI_CHARACTERISTIC_UUID  = '26E2B12B-85F0-4F3F-9FDD-91D114270E6E';
+
+// -------------- //
+// On window load //
+// -------------- //
+window.onload = () => {
+    initializeApp();
+};
+
+
+// ------------ //
+// UI functions //
+// ------------ //
+function uiTemperature(val) {
+    val = val/10;
+    const el = document.getElementById("temperature");
+
+    el.innerText = val;
+    uiMoveMeterGraph(val)
+}
+
+function uiMoveMeterGraph(val){
+var opts = {
+    angle: 0.15, // The span of the gauge arc
+    lineWidth: 0.44, // The line thickness
+    radiusScale: 1, // Relative radius
+    pointer: {
+      length: 0.6, // // Relative to gauge radius
+      strokeWidth: 0.035, // The thickness
+      color: '#000000' // Fill color
+    },
+    limitMax: false,     // If false, max value increases automatically if value > maxValue
+    limitMin: false,     // If true, the min value of the gauge will be fixed
+    colorStart: '#6FADCF',   // Colors
+    colorStop: '#8FC0DA',    // just experiment with them
+    strokeColor: '#E0E0E0',  // to see which ones work best for you
+    generateGradient: true,
+    highDpiSupport: true,     // High resolution support
+    
+  };
+  var target = document.getElementById('graph'); // your canvas element
+  var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
+  gauge.maxValue = 50; // set max gauge value
+  gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+  gauge.animationSpeed = 1; // set animation speed (32 is default value)
+  gauge.set(val); // set actual value
+}
+
+
+function uiToggleDeviceConnected(connected) {
+    const elStatus = document.getElementById("status");
+    const elControls = document.getElementById("controls");
+
+    elStatus.classList.remove("error");
+
+    if (connected) {
+        // Hide loading animation
+        uiToggleLoadingAnimation(false);
+        // Show status connected
+        elStatus.classList.remove("inactive");
+        elStatus.classList.add("success");
+        elStatus.innerText = "Device connected";
+        // Show controls
+        elControls.classList.remove("hidden");
+    } else {
+        // Show loading animation
+        uiToggleLoadingAnimation(true);
+        // Show status disconnected
+        elStatus.classList.remove("success");
+        elStatus.classList.add("inactive");
+        elStatus.innerText = "Device disconnected";
+        // Hide controls
+        elControls.classList.add("hidden");
+    }
+}
+
+function uiToggleLoadingAnimation(isLoading) {
+    const elLoading = document.getElementById("loading-animation");
+
+    if (isLoading) {
+        // Show loading animation
+        elLoading.classList.remove("hidden");
+    } else {
+        // Hide loading animation
+        elLoading.classList.add("hidden");
+    }
+}
+
+function uiStatusError(message, showLoadingAnimation) {
+    uiToggleLoadingAnimation(showLoadingAnimation);
+
+    const elStatus = document.getElementById("status");
+    const elControls = document.getElementById("controls");
+
+    // Show status error
+    elStatus.classList.remove("success");
+    elStatus.classList.remove("inactive");
+    elStatus.classList.add("error");
+    elStatus.innerText = message;
+
+    // Hide controls
+    elControls.classList.add("hidden");
+}
+
+function makeErrorMsg(errorObj) {
+    return "Error\n" + errorObj.code + "\n" + errorObj.message;
+}
+
+// -------------- //
+// LIFF functions //
+// -------------- //
+
+function initializeApp() {
+    liff.init(() => initializeLiff(), error => uiStatusError(makeErrorMsg(error), false));
+}
+
+
+function initializeLiff() {
+    liff.initPlugins(['bluetooth']).then(() => {
+        liffCheckAvailablityAndDo(() => liffRequestDevice());
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+}
+
+function liffCheckAvailablityAndDo(callbackIfAvailable) {
+    // Check Bluetooth availability
+    liff.bluetooth.getAvailability().then(isAvailable => {
+        if (isAvailable) {
+            uiToggleDeviceConnected(false);
+            callbackIfAvailable();
+        } else {
+            uiStatusError("Bluetooth not available", true);
+            setTimeout(() => liffCheckAvailablityAndDo(callbackIfAvailable), 10000);
+        }
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });;
+}
+
+function liffRequestDevice() {
+    liff.bluetooth.requestDevice().then(device => {
+        liffConnectToDevice(device);
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+}
+
+function liffConnectToDevice(device) {
+    device.gatt.connect().then(() => {
+        // document.getElementById("device-name").innerText = device.name;
+        // document.getElementById("device-id").innerText = device.id;
+
+        // Show status connected
+        uiToggleDeviceConnected(true);
+
+        // Get service
+        device.gatt.getPrimaryService(USER_SERVICE_UUID).then(service => {
+            liffGetUserService(service);
+        }).catch(error => {
+            uiStatusError(makeErrorMsg(error), false);
+        });
+        device.gatt.getPrimaryService(PSDI_SERVICE_UUID).then(service => {
+            liffGetPSDIService(service);
+        }).catch(error => {
+            uiStatusError(makeErrorMsg(error), false);
+        });
+
+        // Device disconnect callback
+        const disconnectCallback = () => {
+            // Show status disconnected
+            uiToggleDeviceConnected(false);
+
+            // Remove disconnect callback
+            device.removeEventListener('gattserverdisconnected', disconnectCallback);
+
+            // Reset LED state
+            ledState = false;
+            // Reset UI elements
+            uiToggleLedButton(false);
+            uiToggleStateButton(false);
+
+            // Try to reconnect
+            initializeLiff();
+        };
+
+        device.addEventListener('gattserverdisconnected', disconnectCallback);
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+}
+
+function liffGetUserService(service) {
+    // Button pressed state
+    service.getCharacteristic(BTN_CHARACTERISTIC_UUID).then(characteristic => {
+        liffGetButtonStateCharacteristic(characteristic);
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+
+    // Toggle LED
+    service.getCharacteristic(LED_CHARACTERISTIC_UUID).then(characteristic => {
+        window.ledCharacteristic = characteristic;
+
+        // Switch off by default
+        liffToggleDeviceLedState(false);
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+}
+
+function liffGetPSDIService(service) {
+    // Get PSDI value
+    service.getCharacteristic(PSDI_CHARACTERISTIC_UUID).then(characteristic => {
+        return characteristic.readValue();
+    }).then(value => {
+        // Byte array to hex string
+        const psdi = new Uint8Array(value.buffer)
+            .reduce((output, byte) => output + ("0" + byte.toString(16)).slice(-2), "");
+        //document.getElementById("device-psdi").innerText = psdi;
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+}
+
+function liffGetButtonStateCharacteristic(characteristic) {
+    // Add notification hook for button state
+    // (Get notified when button state changes)
+    characteristic.startNotifications().then(() => {
+        characteristic.addEventListener('characteristicvaluechanged', e => {
+            const buff = (new Uint8Array(e.target.value.buffer));
+            var val = Number((new TextDecoder).decode(buff));
+            console.log(buff);
+            uiTemperature(val);  
+        });
+    }).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+}
+
+function liffToggleDeviceLedState(state) {
+    // on: 0x01
+    // off: 0x00
+    window.ledCharacteristic.writeValue(
+        state ? new Uint8Array([0x01]) : new Uint8Array([0x00])
+    ).catch(error => {
+        uiStatusError(makeErrorMsg(error), false);
+    });
+}
